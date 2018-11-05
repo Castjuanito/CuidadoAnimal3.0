fbd0eb18cca823c8d0b7e72e4fa5b2bd3609236b
oscartorres098 <osdator98@gmail.com>
2018-11-04 20:20:02 -0500

    interfaces de inicio

--- Vista/Login/RegisCliente.js
+++
@@ -1,6 +0,0 @@
-// JavaScript Document
-function mostrarArchivo(){
-	var direccion = document.getElementById('archivoImagen').value;
-	var nombre = direccion.split("\\");
-	document.getElementById('tituloArch').innerHTML = nombre[nombre.length-1];
-}
