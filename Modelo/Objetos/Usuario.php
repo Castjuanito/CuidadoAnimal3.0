b1e13f8cd91d9d9c36a06fb6e603251984b9b727
4Khaos <imroocker@gmail.com>
2018-11-09 13:04:38 -0500

    Usuario

--- Modelo/Objetos/Usuario.php
+++ Modelo/Objetos/Usuario.php
@@ -15,9 +15,8 @@ class Usuario
   private $rol;
   private $conBD;
 
-  function __construct($id, $user_name, $password, $nombre, $apellido, $emailadd, $rol)
+  function __construct($user_name, $password, $nombre, $apellido, $emailadd, $rol)
   {
-    $this->$id = $id;
     $this->$user_name = $user_name;
     $this->$password = $password;
     $this->$nombre = $nombre;
@@ -43,8 +42,204 @@ class Usuario
   }
 
   function UpdateUsuario (){
-    $sql = "UPDATE usuario SET "
+    $sql = "UPDATE usuario SET user_name = ". $this->$user_name . ", password = " . $this->$password . ", nombre = "
+           . $this->$nombre . ", apellido = ". $this->$apellido. ", emailadd = ". $this->emai . ", rol = ". $this->rol .
+           "WHERE usuario.id = ". $this->id;
+    return $this->$conBD->ejecutarconsulta($sql);
   }
+
+  public static function borrarUsuario ()
+  {
+    $sql = "DELETE FROM usuario WHERE usuario.id = $this->$id ";
+    return $this->$conBD->ejecutarconsulta($sql);
+  }
+
+  public static function getById($id)
+  {
+    $sql = "SELECT * FROM usuario WHERE usuario.id = $id";
+    $consulta = $this->$conBD->ejecutarconsulta($sql);
+    $fila = mysqli_fetch_array($consulta);
+    $usuarioObj = new Usuario();
+    $usuarioObj->setId($fila["id"]);
+    $usuarioObj->setPassword($fila["password"]);
+    $usuarioObj->setUserName($fila["user_name"]);
+    $usuarioObj->setApellido($fila["apellido"]);
+    $usuarioObj->setNombre($fila["nombre"]);
+    $usuarioObj->setEmailadd($fila["emailadd"]);
+    $usuarioObj->setRol($fila["rol"]);
+    return $usuarioObj;
+  }
+  public static function getByUsername($user_name)
+  {
+    $sql = "SELECT * FROM usuario WHERE usuario.user_name = $user_name";
+    $consulta = $this->$conBD->ejecutarconsulta($sql);
+    $fila = mysqli_fetch_array($consulta);
+    $usuarioObj = new Usuario();
+    $usuarioObj->setId($fila["id"]);
+    $usuarioObj->setPassword($fila["password"]);
+    $usuarioObj->setUserName($fila["user_name"]);
+    $usuarioObj->setApellido($fila["apellido"]);
+    $usuarioObj->setNombre($fila["nombre"]);
+    $usuarioObj->setEmailadd($fila["emailadd"]);
+    $usuarioObj->setRol($fila["rol"]);
+    return $usuarioObj;
+  }
+
+    /**
+     * Get the value of Id
+     *
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+    /**
+     * Get the value of User Name
+     *
+     * @return mixed
+     */
+    public function getUserName()
+    {
+        return $this->user_name;
+    }
+
+    /**
+     * Set the value of User Name
+     *
+     * @param mixed user_name
+     *
+     * @return self
+     */
+    public function setUserName($user_name)
+    {
+        $this->user_name = $user_name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Password
+     *
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set the value of Password
+     *
+     * @param mixed password
+     *
+     * @return self
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Nombre
+     *
+     * @return mixed
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set the value of Nombre
+     *
+     * @param mixed nombre
+     *
+     * @return self
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Apellido
+     *
+     * @return mixed
+     */
+    public function getApellido()
+    {
+        return $this->apellido;
+    }
+
+    /**
+     * Set the value of Apellido
+     *
+     * @param mixed apellido
+     *
+     * @return self
+     */
+    public function setApellido($apellido)
+    {
+        $this->apellido = $apellido;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Emailadd
+     *
+     * @return mixed
+     */
+    public function getEmailadd()
+    {
+        return $this->emailadd;
+    }
+
+    /**
+     * Set the value of Emailadd
+     *
+     * @param mixed emailadd
+     *
+     * @return self
+     */
+    public function setEmailadd($emailadd)
+    {
+        $this->emailadd = $emailadd;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Rol
+     *
+     * @return mixed
+     */
+    public function getRol()
+    {
+        return $this->rol;
+    }
+
+    /**
+     * Set the value of Rol
+     *
+     * @param mixed rol
+     *
+     * @return self
+     */
+    public function setRol($rol)
+    {
+        $this->rol = $rol;
+
+        return $this;
+    }
+
 }
 
  ?>
