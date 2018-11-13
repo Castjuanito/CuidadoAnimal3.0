563593d51ff9bd407850cff8d3e3cd88e231375f
MiguelBaron <baron.miguel@javeriane.edu.co>
2018-11-13 14:08:59 -0500

    Vistas actualizadas

--- Vista/login.php
+++ Vista/login.php
@@ -6,7 +6,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.css">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<link rel="stylesheet" href="../styles/login.css">
 	<title>Login</title>
@@ -14,22 +14,44 @@
 <body>
 	<!-- Inicio Navbar -->
 	<!--Barra de navegacion-->
-	<nav class="navbar nav-masthead navbar-dark sticky-top navbar-expand-lg text-center barra" id="mainNav">
-    	<a class="navbar-brand mx-auto" href="">
-          <img id="logo" src="../assets/img/logoIngSoft.PNG" height="50" class="d-inline-block align-top" alt="Cuidado animal">
+    <nav class="navbar navbar-expand-xl navbar-dark bg-dark sticky-top">
+    <div class="container-fluid d-flex justify-content-between">
+      <a class="navbar-brand mx-auto" href="#">
+          <img id="logo" src="../assets/img/logoIngSoft.PNG" height="30" class="d-inline-block align-top" alt="Cuidado animal">
       </a>
-      <button class="navbar-toggler collapsed navbar-toggler-right text-center" type="button" data-toggle="collapse" data-target="#navbarTogglerCA" aria-controls="navbarTogglerCA" aria-halflings-expandes="false" aria-label="Toggle navigation">
-          <span class="nav-icon navbar-toggler-icon"></span>
-        </button>
-		<div class="collapse navbar-collapse" id="navbarTogglerCA">
-        <div class="navbar-nav mx-auto text-center">
-            <a class="nav-item text-barra" href="busqueda.php"><img src="../assets/img/buscar-negro.svg" class="d-inline-block align-top mx-1" width="25" height="25">Busqueda</a>
-            <a class="nav-item text-barra" href="login.php"><img src="../assets/img/login-negro.svg" class="d-inline-block align-top mx-1" width="25" height="25">Login</a>
-            <a class="nav-item text-barra" href="registro.php"><img src="../assets/img/registro-negro.svg" class="d-inline-block align-top mx-1" width="25" height="25">Registrarse</a>
-            <a class="nav-item text-barra" href="contacto.php"><img src="../assets/img/contacto-negro.svg" class="d-inline-block align-top mx-1" width="25" height="25">Contactanos</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#MyNavbar" aria-controls="MyNavbar" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="MyNavbar">
+        <div class="navbar-nav mr-auto ml-auto text-center">
+          <a class="nav-item nav-link active" href="#">
+            <div class="d-flex align-items-center justify-content-center">
+                <img src="../assets/img/login-negro.svg" class="d-inline-block align-top mx-1" width="25" height="25">
+                Login
+            </div>
+          </a>
+          <a class="nav-item nav-link" href="busqueda.php">
+            <div class="d-flex align-items-center justify-content-center">
+                <img src="../assets/img/buscar-negro.svg" class="d-inline-block align-top mx-1" width="25" height="25">
+                Buscar
+            </div>
+          </a>
+          <a class="nav-item nav-link" href="registro.php">
+            <div class="d-flex align-items-center justify-content-center">
+                <img src="../assets/img/registro-negro.svg" class="d-inline-block align-top mx-1" width="25" height="25">
+                Registrarse
+            </div>
+          </a>
+          <a class="nav-item nav-link" href="contacto.php">
+            <div class="d-flex align-items-center justify-content-center">
+                <img src="../assets/img/contacto-negro.svg" class="d-inline-block align-top mx-1" width="25" height="25">
+                Contactanos
+            </div>
+          </a>
         </div>
-    	</div>
-	</nav>
+      </div>
+    </div>
+    </nav>
 	<div class="container">
 		<div class="col-md-4 col-sm-12 fondo1">
 			<p class="text-center titulo">Cuidado Animal</p>
@@ -51,7 +73,9 @@
     			<label for="palabraFormText">Contraseña</label>
     			<input class="form-control" id="palabraFormControlInput" placeholder="Contraseña" name="password">
  		 	</div>
- 		 	<button type="submit" class="btn btn-primary btn-buscar" name="ingresar">Ingresar</button>
+            <div class="d-flex justify-content-end">
+ 		 		<button type="submit" class="btn btn-primary" name="ingresar">Ingresar</button>
+            </div>
   		</div>
       </form>
   	</div>
