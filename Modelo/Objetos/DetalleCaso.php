63d46b354e028859fd7fd5a82d4df691f4ae2e91
4Khaos <imroocker@gmail.com>
2018-11-13 19:48:09 -0500

    mostrar casos asociados a veterinario y sus detalles

--- Modelo/Objetos/DetalleCaso.php
+++ Modelo/Objetos/DetalleCaso.php
@@ -31,9 +31,32 @@ class DetalleCaso{
 
   public function borrarDetalleCaso()
   {
+    $conBD = new conexion();
     $sql = "DELETE FROM detalle_caso WHERE detalle_caso.id = ". $this->id;
     return $conBD->ejecutarconsulta($sql);
   }
 
+  public function getById($id)
+  {
+    $conBD = new conexion();
+    $sql = "SELECT * FROM DETALLE_CASO WHERE DETALLE_CASO.ID =".$id;
+    $consulta = $conBD->ejecutarconsulta($sql);
+    if (!($consulta->num_rows < 1))
+    {
+      $fila = mysqli_fetch_array($consulta);
+      $detalle = new DetalleCaso($fila['caso_id'],$fila['info']);
+      $detalle->setFecha($fila['fecha']);
+      $detalle->setId($fila['id']);
+      return $detalle;
+    }else {
+      return false;
+    }
+  }
+  public function getByCaso($id_caso)
+  {
+    $conBD = new conexion();
+    $sql = "SELECT * FROM DETALLE_CASO WHERE DETALLE_CASO.CASO_ID =".$id_caso;
+    return $conBD->ejecutarconsulta($sql);
+  }
 }
 ?>
